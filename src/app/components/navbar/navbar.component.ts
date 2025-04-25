762c1ab3e9f7038786399fb940b133a2279e7433
AhmedZahran15 <ahmedzhran876@gmail.com>
2025-04-25 23:32:57 +0300

    Add navbar component with HTML, CSS, and TypeScript files

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MenubarModule } from 'primeng/menubar';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [RouterLink, RouterLinkActive, MenubarModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css',
+})
+export class NavbarComponent {}

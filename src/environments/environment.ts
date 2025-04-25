adbb2f8992fbb1fab03700672536dbf92b198773
AhmedZahran15 <ahmedzhran876@gmail.com>
2025-04-25 21:57:45 +0300

    Add environment configuration files for production and development

---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  tmdb_api_key: 'e8b49ed75df679406613a64060211486',
+  tmdb_read_access_token:
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlOGI0OWVkNzVkZjY3OTQwNjYxM2E2NDA2MDIxMTQ4NiIsIm5iZiI6MTc0NTYwNTg1OC42MDcsInN1YiI6IjY4MGJkNGUyNDFhMzgyZDZhMDg5ZDBiNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.7L-Td-ip-Lw-E_dy_WQq4zusOV7wg3erCMm7WJOsIcs',
+};

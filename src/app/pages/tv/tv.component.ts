e14d5b91c43bed539fcf2c0e322351850a5ed630
AhmedZahran15 <ahmedzhran876@gmail.com>
2025-04-26 05:25:03 +0300

    Refactor TvComponent to use typed Tv array and improve getTvShows method structure

--- src/app/pages/tv/tv.component.ts
+++ src/app/pages/tv/tv.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TmdbService } from '../../tmdb.service';
 import { MediaCardComponent } from '../../components/media-card/media-card.component';
+import { Tv } from '../../interfaces/tv';
 
 @Component({
   selector: 'app-tv',
@@ -10,17 +11,24 @@ import { MediaCardComponent } from '../../components/media-card/media-card.compo
   styleUrl: './tv.component.css',
 })
 export class TvComponent implements OnInit {
-  tvShows: any[] = [];
+  tvShows: Tv[] = [];
   imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
   tmdbService = inject(TmdbService);
   router = inject(Router);
   ngOnInit(): void {
-    this.tmdbService.getTrendingTv().subscribe((data: any) => {
-      this.tvShows = data.results;
+    this.getTvShows();
+  }
+  getTvShows(): void {
+    this.tmdbService.getTrendingTv().subscribe({
+      next: (data: any) => {
+        this.tvShows = data.results;
+      },
+      error: (error) => {
+        console.error('Error fetching TV shows:', error);
+      },
     });
   }
-
-  navigateToDetails(show: any): void {
+  navigateToDetails(show: Tv): void {
     this.router.navigate(['/tv', show.id]);
   }
 }

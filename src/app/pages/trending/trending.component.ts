4cf09b56577ca19180ef240220b7c3b6a4d83c8d
AhmedZahran15 <ahmedzhran876@gmail.com>
2025-04-26 05:25:10 +0300

    Refactor TrendingComponent to use typed arrays for trending items and improve getTrending method structure

--- src/app/pages/trending/trending.component.ts
+++ src/app/pages/trending/trending.component.ts
@@ -3,27 +3,37 @@ import { Router } from '@angular/router';
 import { TmdbService } from '../../tmdb.service';
 import { MediaCardComponent } from '../../components/media-card/media-card.component';
 import { PersonCardComponent } from '../../components/person-card/person-card.component';
+import { Movie } from '../../interfaces/movie';
+import { Person } from '../../interfaces/person';
+import { Tv } from '../../interfaces/tv';
 
 @Component({
   selector: 'app-trending',
   standalone: true,
-  imports: [ MediaCardComponent, PersonCardComponent],
+  imports: [MediaCardComponent, PersonCardComponent],
   templateUrl: './trending.component.html',
   styleUrl: './trending.component.css',
 })
 export class TrendingComponent implements OnInit {
-  trending: any[] = [];
+  trending: (Movie | Person | Tv)[] = [];
   imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
   tmdbService = inject(TmdbService);
   router = inject(Router);
 
   ngOnInit(): void {
-    this.tmdbService.getAllTrending().subscribe((data: any) => {
-      this.trending = data.results;
+    this.getTrending();
+  }
+  getTrending(): void {
+    this.tmdbService.getAllTrending().subscribe({
+      next: (data: any) => {
+        this.trending = data.results;
+      },
+      error: (error) => {
+        console.error('Error fetching trending data:', error);
+      },
     });
   }
-
-  navigateToDetails(item: any): void {
+  navigateToDetails(item: Movie | Tv | Person): void {
     const type = item.media_type;
     const id = item.id;
     if (type === 'movie') {

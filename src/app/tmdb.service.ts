690ee77abd507affcfe9bf20a56ddecc80c71006
AhmedZahran15 <ahmedzhran876@gmail.com>
2025-04-26 05:23:41 +0300

    Refactor TmdbService methods to specify return types as Observable<any>

--- src/app/tmdb.service.ts
+++ src/app/tmdb.service.ts
@@ -1,31 +1,45 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
+import { Observable } from 'rxjs';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class TmdbService {
   private http = inject(HttpClient);
-  getAllTrending() {
-    return this.http.get(`https://api.themoviedb.org/3/trending/all/day?language=en-US&api_key=${environment.tmdb_api_key}`)
+  getAllTrending(): Observable<any> {
+    return this.http.get(
+      `https://api.themoviedb.org/3/trending/all/day?language=en-US&api_key=${environment.tmdb_api_key}`
+    );
   }
-  getTrendingMovies() {
-    return this.http.get(`https://api.themoviedb.org/3/trending/movie/day?language=en-US&api_key=${environment.tmdb_api_key}`)
+  getTrendingMovies(): Observable<any> {
+    return this.http.get(
+      `https://api.themoviedb.org/3/trending/movie/day?language=en-US&api_key=${environment.tmdb_api_key}`
+    );
   }
-  getTrendingTv() {
-    return this.http.get(`https://api.themoviedb.org/3/trending/tv/day?language=en-US&api_key=${environment.tmdb_api_key}`)
+  getTrendingTv(): Observable<any> {
+    return this.http.get(
+      `https://api.themoviedb.org/3/trending/tv/day?language=en-US&api_key=${environment.tmdb_api_key}`
+    );
   }
-  getTrendingPeople() {
-    return this.http.get(`https://api.themoviedb.org/3/trending/person/day?language=en-US&api_key=${environment.tmdb_api_key}`)
+  getTrendingPeople(): Observable<any> {
+    return this.http.get(
+      `https://api.themoviedb.org/3/trending/person/day?language=en-US&api_key=${environment.tmdb_api_key}`
+    );
   }
-  getMovieDetails(id: number) {
-    return this.http.get(`https://api.themoviedb.org/3/movie/${id}?language=en-US&api_key=${environment.tmdb_api_key}`)
+  getMovieDetails(id: number): Observable<any> {
+    return this.http.get(
+      `https://api.themoviedb.org/3/movie/${id}?language=en-US&api_key=${environment.tmdb_api_key}`
+    );
   }
-  getTvDetails(id: number) {
-    return this.http.get(`https://api.themoviedb.org/3/tv/${id}?language=en-US&api_key=${environment.tmdb_api_key}`)
+  getTvDetails(id: number): Observable<any> {
+    return this.http.get(
+      `https://api.themoviedb.org/3/tv/${id}?language=en-US&api_key=${environment.tmdb_api_key}`
+    );
   }
-  getPersonDetails(id: number) {
-    return this.http.get(`https://api.themoviedb.org/3/person/${id}?language=en-US&api_key=${environment.tmdb_api_key}`)
+  getPersonDetails(id: number): Observable<any> {
+    return this.http.get(
+      `https://api.themoviedb.org/3/person/${id}?language=en-US&api_key=${environment.tmdb_api_key}`
+    );
   }
 }
